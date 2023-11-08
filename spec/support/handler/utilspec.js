9ab29502ecead8fdb6c83a98855c752ec88e23a8
Vince <vincent.taylorcode@gmail.com>
2023-11-08 16:54:41 -0500

    Updated key generation across all handlers

--- spec/support/handler/utilspec.js
+++ spec/support/handler/utilspec.js
@@ -1,23 +1,26 @@
-const generateShortUUID = require("../../../lib/main/util/util.js");
+const Util = require("../../../lib/main/util/util.js");
+const ServerUtil = require("../../../lib/main/util/server-utils.js");
+const { ValidationException } = require("../../../lib/main/util/exceptions.js");
+
 describe("UtilTester", () => {
 
-    it("IDs should be the same length", () => {
-        var id1 = generateShortUUID.generateShortUUID();
-        var id2 = generateShortUUID.generateShortUUID();
-        var id3 = generateShortUUID.generateShortUUID();
-        var id4 = generateShortUUID.generateShortUUID();
-        var id5 = generateShortUUID.generateShortUUID();
-        var id6 = generateShortUUID.generateShortUUID();
+    it("IDs should be number", () => {
+        var id1 = Util.generateShortUUID();
+        var id2 = Util.generateShortUUID();
 
-        expect(id1.length).toEqual(22);
-        expect(id2.length).toEqual(22);
-        expect(id3.length).toEqual(22);
-        expect(id4.length).toEqual(22);
-        expect(id5.length).toEqual(22);
-        expect(id6.length).toEqual(22);
-
-
+        expect(typeof id1).toEqual(typeof Number(0));
+        expect(typeof id2).toEqual(typeof Number(0));
     });
+
+    it("Ids should be 22 length number", () => {
+        var id1 = "12313123123";
+        var id2 = "1234567890123456789012";
+        var id3 = "13248970000000823094871293847";
+
+        expect(ServerUtil.validateAndConvertId(id2)).toEqual(1234567890123456789012);
+        expect(function(){ServerUtil.validateAndConvertId(id1)}).toThrow(new ValidationException(`Id ${id1} is not valid id`));
+        expect(function(){ServerUtil.validateAndConvertId(id3)}).toThrow(new ValidationException(`Id ${id3} is not valid id`));
+    })
 
 
 });
